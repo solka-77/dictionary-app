b0fed00514dbf8e4e3e5b7340c1a839e01e8d435
solka-77 <knastaran715@gmail.com>
2022-06-13 12:57:26 +0430

    Renderd-words-meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,20 +4,19 @@ import Results from "./Results";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
-  function search(event) {
-    event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
-  }
-  function handleKeywordChange(event) {
-    console.log(event.target.value);
-    setKeyword(event.target.value);
-  }
   function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
+    console.log(response.data[0]);
     setResults(response.data[0]);
   }
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Meaning from "./Meaning";
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.meaning.map(function (meaning, index) {
-          return meaning.definitions[0].definitions;
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
